af74732f7b8d1ea0caf2b1de26811e3985fafece
deror1869107 <sungyuanyao@gmail.com>
2014-12-03 18:46:41 +0800

    m7ul

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"https://github.com/firefoxos-htc-one/b2g-manifest.git"}
+GITREPO=${GITREPO:-"https://github.com/deror1869107/pure-b2g.git"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
@@ -71,95 +71,16 @@ echo GECKO_OBJDIR=$PWD/objdir-gecko >> .tmp-config
 echo DEVICE_NAME=$1 >> .tmp-config
 
 case "$1" in
-"galaxy-s2")
-	echo DEVICE=galaxys2 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"galaxy-nexus")
-	echo DEVICE=maguro >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-4")
-	echo DEVICE=mako >> .tmp-config &&
-	repo_sync nexus-4
-	;;
-
 "m7ul")
 	echo DEVICE=m7ul >> .tmp-config &&
 	repo_sync m7ul
 	;;
-
-"optimus-l5")
-	echo DEVICE=m4 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-s")
-	echo DEVICE=crespo >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-s-4g")
-	echo DEVICE=crespo4g >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"otoro"|"unagi"|"keon"|"inari"|"leo"|"hamachi"|"peak"|"helix"|"wasabi")
-	echo DEVICE=$1 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"tara")
-	echo DEVICE=sp8810ea >> .tmp-config &&
-	echo LUNCH=sp8810eabase-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"pandaboard")
-	echo DEVICE=panda >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"emulator"|"emulator-jb")
-	echo DEVICE=generic >> .tmp-config &&
-	echo LUNCH=full-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"emulator-x86"|"emulator-x86-jb")
-	echo DEVICE=generic_x86 >> .tmp-config &&
-	echo LUNCH=full_x86-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
 *)
 	echo "Usage: $0 [-cdflnq] (device name)"
 	echo "Flags are passed through to |./repo sync|."
 	echo
 	echo Valid devices to configure are:
-	echo - galaxy-s2
-	echo - galaxy-nexus
-	echo - nexus-4
 	echo - m7ul
-	echo - nexus-s
-	echo - nexus-s-4g
-	echo - otoro
-	echo - unagi
-	echo - inari
-	echo - keon
-	echo - peak
-	echo - leo
-	echo - hamachi
-	echo - helix
-	echo - wasabi
-	echo - tara
-	echo - pandaboard
-	echo - emulator
-	echo - emulator-jb
-	echo - emulator-x86
-	echo - emulator-x86-jb
 	exit -1
 	;;
 esac
